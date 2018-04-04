c72aa746d9ed7df06cc7f5ed363e9cc229c08357
Circoolbilog <clarkllarena@ymail.com>
2018-04-04 17:23:25 +0800

    added nodes

---
+++ SwitchBits/Assets/Scripts/NODES.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class NODES : MonoBehaviour {
+
+    public Transform cameraPosition;
+
+    public List<NODES> reachableNodes = new List<NODES>();
+
+    [HideInInspector]
+    public Collider col;
+
+
+	// Use this for initialization
+	void Start () {
+        col = GetComponent<Collider>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

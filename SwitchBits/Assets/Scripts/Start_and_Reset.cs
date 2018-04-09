53fbee3eb10c6ba3ae6ede8a2d9720d0f7ed4067
Circoolbilog <clarkllarena@ymail.com>
2018-04-09 19:23:24 +0800

    wrist hurts af

---
+++ SwitchBits/Assets/Scripts/Start_and_Reset.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_and_Reset : MonoBehaviour
+{
+    public GameObject pusher;
+    bool simStart = false;
+    public float moveSpeed = 60f;
+
+    void Update()
+    {
+        
+        bool down = Input.GetButtonDown("Jump");
+        if (down)
+        {
+            StartR();
+        }
+    }
+    public void StartR()
+    {
+        if (simStart == false)
+        {
+            simStart = true;
+            Debug.Log(simStart);
+            pusher.transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime);
+        }
+        else if (simStart == true)
+        {
+            simStart = false;
+            Debug.Log(simStart);
+
+        }
+    }
+
+}
--- SwitchBits/Assets/Scripts/Switch_Toggle.cs
+++ SwitchBits/Assets/Scripts/Switch_Toggle.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Switch_Toggle : MonoBehaviour {
+public class Switch_Toggle : MonoBehaviour
+{
+    public bool toggle = false;
+    public float turnSpeed = 100f;
+    public GameObject other;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("mouse clicked");
+
+        if (toggle == false)
+        {
+            transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+            toggle = true;
+            Debug.Log(toggle);
+            other.SetActive(true);
+
+        }
+
+        else if (toggle == true)
+        {
+            transform.Rotate(-Vector3.forward, turnSpeed * Time.deltaTime);
+            toggle = false;
+            Debug.Log(toggle);
+            other.SetActive(false);
+
+        }
+    }
+   
 }

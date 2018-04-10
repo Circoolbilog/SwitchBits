116aa902ab7a4a2b0b711e29e81f89ad1cddb633
Circoolbilog <clarkllarena@ymail.com>
2018-04-10 10:55:44 +0800

    asfsadfag

--- SwitchBits/Assets/Scripts/Start_and_Reset.cs
+++ SwitchBits/Assets/Scripts/Start_and_Reset.cs
@@ -1,39 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Start_and_Reset : MonoBehaviour
 {
     public GameObject pusher;
-    bool simStart = false;
+    public GameObject pointLight;
     public float moveSpeed = 60f;
+    public float fractionWay;
+    bool simStart = false;
     bool move = false;
+    private Scene scene;
+    Vector3 startPos;
+    Vector3 endPos;
+
+
+    private void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+        startPos = transform.position;
+        endPos = transform.position + new Vector3(10,0,0);
+    }
 
     void Update()
     {
-        
-        bool down = Input.GetButtonDown("Jump");
+       
+        bool down = Input.GetButtonDown("Jump");            //checks if space is pressed
         if (down)
         {
             StartR();
         }
         if (move == true)
         {
-            pusher.transform.position += transform.right * Time.deltaTime;
+          
+            //pusher.transform.position += -transform.right * Time.deltaTime;
+            fractionWay += 0.01f;
+            pusher.transform.position = Vector3.Lerp(startPos, endPos, fractionWay);  //will push the pusher to start the level
+
         }
     }
-    public void StartR()
+    
+    public void StartR()        //will start or reset the level
     {
-        if (simStart == false)
+        if(pointLight.activeSelf == true)
         {
-            simStart = true;
-            Debug.Log(simStart);
-            move = true;
+            if (simStart == false)
+            {
+                simStart = true;
+                Debug.Log(simStart);
+                move = true;
+            }
+            else if (simStart == true)
+            {
+                simStart = false;
+                Debug.Log(simStart);
+                SceneManager.LoadScene(scene.name);
+
+            }
         }
         else if (simStart == true)
         {
             simStart = false;
             Debug.Log(simStart);
+            SceneManager.LoadScene(scene.name);
 
         }
     }

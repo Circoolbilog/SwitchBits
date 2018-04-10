6301897ba9070248260cb4422a2d0a6ce7917b59
Circoolbilog <clarkllarena@ymail.com>
2018-04-10 14:36:22 +0800

    ui and levels left

--- SwitchBits/Assets/Scripts/trigger.cs
+++ SwitchBits/Assets/Scripts/trigger.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class trigger : MonoBehaviour {
+public class trigger : MonoBehaviour
+{
+    public GameObject WinScreen;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        WinScreen.SetActive(true);
+    }
 }
